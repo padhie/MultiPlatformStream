da866bf900f44432fa011182095f0bb1eca42f96
padhie <padhiez@hotmail.de>
2017-10-31 00:41:15 +0100

    Init

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Madcoda\Youtube\Youtube;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function indexAction(Request $request)
+    {
+    	
+    	if ($request->get("submit") == "1") {
+			$platformList = $request->get("platform");
+			$streamList = $request->get("stream");
+		
+			$streamString = "";
+			foreach ($streamList AS $index => $stream) {
+				if (!empty($stream)) {
+					
+					$tmpStreamString = "";
+					
+					switch ($platformList[$index]) {
+						case "youtube":
+							$tmpStreamString .= "yt:";
+							break;
+						case "twitch":
+							$tmpStreamString .= "tw:";
+							break;
+					}
+					
+					$tmpStreamString .= urlencode($stream);
+					$streamString .= $tmpStreamString."/";
+				}
+			}
+		
+			if (strlen($streamString) > 0) {
+				$streamString = substr($streamString, 0, -1);
+				$streamString = array_unique($streamString);
+				dump($streamString);
+				return $this->redirectToRoute("stream_view", ["streams" => $streamString]);
+			}
+   
+		}
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig');
+    }
+    
+	/**
+	 * @Route("/{streams}/", name="stream_view", requirements={"streams"=".+"})
+	 */
+	public function streamViewIndex($streams)
+	{
+		$apiKey = $this->container->getParameter('youtube_apikey');
+		$youtube = new Youtube(['key' => $apiKey]);
+		
+		$streamList = [];
+		$streamParameter = array_unique(explode("/", $streams));
+		foreach ($streamParameter AS $streamanem) {
+			if ($streamanem != "") {
+				
+				$explodeStream = explode(":", $streamanem);
+				// Youtube
+				if ($explodeStream[0] == "yt") {
+					$apiData = $youtube->getChannelByName($explodeStream[1]);
+					if (isset($apiData->id)) {
+						$streams = $youtube->searchChannelLiveStream($explodeStream[1], $apiData->id);
+						if (isset($streams[0]) && isset($streams[0]->id) && isset($streams[0]->id->videoId)) {
+							$streamList[] =  [
+								"source" => "youtube",
+								"row" => $explodeStream[1],
+								"channel" => $apiData->id,
+								"stream" => "https://www.youtube.com/embed/live_stream?channel=".$apiData->id,
+								"chat" => "https://www.youtube.com/live_chat?embed_domain=".$_SERVER['SERVER_NAME']."&v=".$streams[0]->id->videoId
+							];
+						}
+					}
+				}
+				// Twitch
+				elseif ($explodeStream[0] == "tw") {
+					$streamList[] =  [
+						"source" => "twitch",
+						"row" => $explodeStream[1],
+						"channel" => $explodeStream[1],
+						"stream" => "http://twitch.tv/".$explodeStream[1]."/embed?muted=true",
+						"chat" => "https://www.twitch.tv/".$explodeStream[1]."/chat"
+					];
+				}
+			}
+		}
+		
+		if (count($streamList) > 0) {
+			return $this->render(
+				"default/stream.html.twig", [
+				"streamData" => $streamList
+			]);
+		} else {
+			return $this->redirectToRoute("home");
+		}
+	}
+}
